cf2512871f04f8e62ca405d75a115cb612b2d61a
JavierClairvaux <javcabr@gmail.com>
2020-06-11 16:14:19 -0700

    updated cpu handlers

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"net/http"
 	"github.com/JavierClairvaux/px-usewithcare/cpuburner"
 	"github.com/JavierClairvaux/px-usewithcare/memeater"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-
 func main() {
 	r := mux.NewRouter()
 	m := memeater.MemEater{}
-	c := cpuburner.CPUBurner{}
+	c := cpuburner.NewCPUBurnerHandler()
+	c.RemoveStoppedJobs()
 
 	//mem handlers
 	r.HandleFunc("/memeater", m.MemGetHandler).Methods("GET")
@@ -20,9 +20,9 @@ func main() {
 	r.HandleFunc("/memeater/free", m.CleanUpMemory).Methods("GET")
 
 	//CPU handlers
-	r.HandleFunc("/cpuburner", c.CPUBurnerHandler).Methods("GET")
-	r.HandleFunc("/cpuburner/start", c.CPUStartHandler).Methods("GET")
-	r.HandleFunc("/cpuburner/stop", c.CPUStopHandler).Methods("GET")
+	r.HandleFunc("/cpuburner/{id}", c.CPUBurnerHandler).Methods("GET")
+	r.HandleFunc("/cpuburner/start", c.CPUStartHandler).Methods("POST")
+	r.HandleFunc("/cpuburner/stop/{id}", c.CPUStopHandler).Methods("DELETE")
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", r)
 }
