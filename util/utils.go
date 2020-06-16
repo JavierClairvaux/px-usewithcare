2e26a59c082182e2671c3a0496881d05cf5e1d56
JavierClairvaux <javcabr@gmail.com>
2020-06-16 09:25:30 -0700

    func returning a marshaled list

--- util/utils.go
+++ util/utils.go
@@ -6,9 +6,20 @@ type HTTPError struct {
 	Error string `json:"error"`
 }
 
+type IDs struct {
+	IDs []string `json:"IDs"`
+}
+
 func GetHTTPError(s string) ([]byte, error) {
 	h := &HTTPError{
 		Error: s,
 	}
 	return json.Marshal(h)
+}
+
+func GetIDs(s []string) ([]byte, error) {
+	i := &IDs{
+		IDs: s,
+	}
+	return json.Marshal(i)
 }
